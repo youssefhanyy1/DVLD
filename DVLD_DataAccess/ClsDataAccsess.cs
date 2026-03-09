2d81a4ad2ada9a4da9595dc72991a5aef25c1b96
youssef hany <hany67334@gmail.com>
2026-03-10 01:14:27 +0200

    refactor: centralize database connection string in App.config

--- DVLD_DataAccess/ClsDataAccsess.cs
+++ DVLD_DataAccess/ClsDataAccsess.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace DVLD_DataAccess
 {
-    public static class ClsDataAccsess// خلي الكلاس public static عشان تعرف توصل للمتغير من أي مكان
+    public static class ClsDataAccsess
     {
-        public static string connations = @"Data Source=DESKTOP-8K82TVQ;Initial Catalog=DVLD;Integrated Security=True;TrustServerCertificate=True;";
+        public static readonly string connations = ConfigurationManager.ConnectionStrings["MyDbConnection"].ConnectionString;
     }
 }
